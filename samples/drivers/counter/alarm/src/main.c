69df47da45d6531587ffbeb0a093f08c0c1cf2f2
Frederik Wenigwieser <zephyr@frederik.at>
2025-07-11 13:38:52 +0200

    gpio toggle works

--- samples/drivers/counter/alarm/src/main.c
+++ samples/drivers/counter/alarm/src/main.c
@@ -9,6 +9,7 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/counter.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
 
 // #define DELAY 2000000
 #define DELAY 200000
@@ -68,6 +69,9 @@ struct counter_alarm_cfg alarm_cfg;
 #error Unable to find a counter device node in devicetree
 #endif
 
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 static void test_counter_interrupt_fn(const struct device *counter_dev,
 				      uint8_t chan_id, uint32_t ticks,
 				      void *user_data)
@@ -77,6 +81,7 @@ static void test_counter_interrupt_fn(const struct device *counter_dev,
 	uint64_t now_usec;
 	int now_sec;
 	int err;
+	int ret = 0;
 
 	err = counter_get_value(counter_dev, &now_ticks);
 	if (!counter_is_counting_up(counter_dev)) {
@@ -88,14 +93,6 @@ static void test_counter_interrupt_fn(const struct device *counter_dev,
 		return;
 	}
 
-	now_usec = counter_ticks_to_us(counter_dev, now_ticks);
-	now_sec = (int)(now_usec / USEC_PER_SEC);
-
-	printk("!!! Alarm !!!\n");
-	printk("Now: %u\n", now_sec);
-
-	/* Set a new alarm with a double length duration */
-	// config->ticks = config->ticks * 2U;
 
 	printk("Set alarm in %u sec (%u ticks)\n",
 	       (uint32_t)(counter_ticks_to_us(counter_dev,
@@ -107,14 +104,31 @@ static void test_counter_interrupt_fn(const struct device *counter_dev,
 	if (err != 0) {
 		printk("Alarm could not be set\n");
 	}
+
+	ret = gpio_pin_toggle_dt(&led);
+	if (ret < 0) {
+		return 0;
+	}
+
 }
 
 int main(void)
 {
 	const struct device *const counter_dev = DEVICE_DT_GET(TIMER);
 	int err;
+	int ret;
 
 	printk("Counter alarm sample\n\n");
+
+	if (!gpio_is_ready_dt(&led)) {
+		printk("gpio device not ready.\n");
+		return 0;
+	}
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		printk("gpio_pin_configure_dt fail\n");
+		return 0;
+	}
 
 	if (!device_is_ready(counter_dev)) {
 		printk("device not ready.\n");
